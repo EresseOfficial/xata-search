451e0b167ae4f2fb224e995ec95e18263040eb21
EresseOfficial <eressemusic01@hotmail.com>
2023-12-15 14:08:13 +0100

    adding e2e tests

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://f38980d25e0a5092e6f35c57a530f77b@o4506388279590912.ingest.sentry.io/4506399523209216",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
